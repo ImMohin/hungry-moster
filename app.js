388b195c0666992070d27bfb1215bbb50f4b0d5e
ImMohin <mohinuddin252@gmail.com>
2021-02-07 11:47:13 +0600

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
 //https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 //https://www.themealdb.com/api/json/v1/1/search.php?f=a
 
@@ -15,9 +15,20 @@ const displayMeals = meals => {
     // for loop use by access all meals
     for (let i = 0; i < meals.meals.length; i++) {
         const meal = meals.meals[i];
-        console.log(meal.strMeal);
+        // console.log(meal.strMeal);
+        const mealDiv = document.createElement('div');
+        //add class
+        mealDiv.className = 'meal';
 
+        const mealInfo = `
+            <div onclick></div>
+            <h3 class="meal-name">${meal.strMeal}</h3>
+            <button onclick="displayMealDetail('${meal.strMeal}')">Details</button>
+        `;
 
+        mealDiv.innerHTML = mealInfo;
+        //meals div under meal div
+        mealsDiv.appendChild(mealDiv);
         
      }
 }
