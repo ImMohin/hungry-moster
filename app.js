189885f7c86851a001b93a6ea0363f24d46bd654
ImMohin <mohinuddin252@gmail.com>
2021-02-07 10:50:29 +0600

    update meals access for loop

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
 //https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
 //https://www.themealdb.com/api/json/v1/1/search.php?f=a
 
 .then(response => response.json())
-.then(data => displayCategories(data));
+.then(data => displayMeals(data));
 
 // all meals get 
-const displayCategories = categories => {
+const displayMeals = meals => {
     //console.log(meals.meals[0]);
     
     // Read Id html div 
-    const categoriesDiv = document.getElementById('categories');
+    const mealsDiv = document.getElementById('meals');
     
     // for loop use by access all meals
-    for (let i = 0; i < categories.categories.length; i++) {
-        const categorie = categories.categories[i];
-        console.log(categorie.strCategory);
+    for (let i = 0; i < meals.meals.length; i++) {
+        const meal = meals.meals[i];
+        console.log(meal.strMeal);
+
 
         
      }
